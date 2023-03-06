b9b6301733b3c5ec07a09dd767f58a9bae400b54
伊藤健太朗 /Kentaro Ito <57889083+kentaro-maker@users.noreply.github.com>
2023-03-07 00:22:05 +0900

    Add function outputs json file

--- getIssue100.py
+++ getIssue100.py
@@ -3,6 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 import random
 import pprint
+import os
+import json
+import codecs
+
 
 
 def neetConnect(url):
@@ -12,7 +16,7 @@ def neetConnect(url):
 
   for i in range(3):
     try:
-      random_sleep_link = random.uniform(5, 10) 
+      random_sleep_link = random.uniform(2, 5) 
       print("Sleep for " + str(random_sleep_link) + " Seconds...")
       time.sleep(random_sleep_link)
       print("Now Requesting to  [" + url + " ] ...")
@@ -32,10 +36,21 @@ def neetConnect(url):
   else: #if x amount of retries on the try-part don't work...#
     raise Exception("Something really went wrong here... I'm sorry.") #...raise an exception and stop the script#
 
+
 def getIssueObj(soup):
   print("Parsing HTML...")
   card_box = soup.find('div', {'id': 'cardBox'})
-  author = card_box.find('td', {'class': 'name'}).text
+  author = {
+    "name": card_box.select_one('div.imgColumn > table > tr:nth-child(2) > td:nth-child(1)').text,
+    "yomi": card_box.select_one('div.imgColumn > table > tr:nth-child(1) > th:nth-child(1)').text,
+    "gender": card_box.select_one('div.imgColumn > table > tr:nth-child(2) > td:nth-child(2)').text,
+    "profile": card_box.select_one('div.imgColumn > p:nth-child(4)').text,
+    "interpretation": {
+      "name": card_box.select_one('div.imgColumn > p:nth-child(5)').text,
+      "profile": card_box.select_one('div.imgColumn > p:nth-child(6)').text,
+    }
+  }
+
   textColumn = card_box.find('div', {'class': 'textColumn'})
   ddAll = textColumn.find_all('dd')
 
@@ -61,11 +76,17 @@ def getIssueObj(soup):
   return issue
 
 
+def outputJson(filename, data):
+  cwd = os.getcwd()
+  path = cwd + '\\' + filename
+  with codecs.open(path , 'w', 'utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=2)
+
 url = 'https://www.samac.jp/search/poems_detail.php?id='
 
 data = []
 
-for i in range(1, 3):
+for i in range(1, 101):
     r = neetConnect(url + str(i))
     if not r:
        continue
@@ -75,6 +96,8 @@ for i in range(1, 3):
     data.append(issue)
     print(str(i) + "-issue is appended.")
 
+result = { "data": data }
+outputJson('data.json', result)
 
 pprint.pprint(data)
 
